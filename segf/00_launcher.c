926e38baf9165934f38fbd5444bb2142807643d6
Jérémie Martel <42200520+jeremiemartel@users.noreply.github.com>
2019-03-25 15:00:58 +0100

    Update 00_launcher.c
    Orthographic correction

--- segf/00_launcher.c
+++ segf/00_launcher.c
@@ -22,7 +22,7 @@ int			segf_launcher(void)
 	load_test(&start, "segf1 : maj conv                      ", &(segf1));
 	load_test(&start, "segf2 : NULL param (optionnal)        ", &(segf2));
 	load_test(&start, "segf3 : too much params               ", &(segf3));
-	load_test(&start, "segf4 : not enought params (optionnal)", &(segf4));
+	load_test(&start, "segf4 : not enough params (optionnal)", &(segf4));
 	load_test(&start, "segf5 : NULL param (optionnal)        ", &(segf5));
 	return (launch_test(start));	
 }
